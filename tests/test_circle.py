8933480f34b7db63fbafb30d9f19b098121780f2
Vlad Krivulets <102356010+Benti261@users.noreply.github.com>
2024-12-27 01:38:05 +0300

    Update test_circle.py

--- tests/test_circle.py
+++ tests/test_circle.py
@@ -1,16 +1,13 @@
-import pytest
+
+import math
 from ..circle import area, perimeter
 
-def test_area_positive():
-    assert area(4) == 16
+def test_area():
+    assert area(1) == math.pi
+    assert area(0) == 0
+    assert area(2.5) == math.pi * (2.5 ** 2)
 
-def test_area_negative():
-    with pytest.raises(ValueError):
-        area(-4)
-
-def test_perimeter_positive():
-    assert perimeter(4) == 16
-
-def test_perimeter_negative():
-    with pytest.raises(ValueError):
-        perimeter(-4)
+def test_perimeter():
+    assert perimeter(1) == 2 * math.pi
+    assert perimeter(0) == 0
+    assert perimeter(2.5) == 2 * math.pi * 2.5
