d75c4c5379ca80d303914417b8696e6bff11b563
Vlad Krivulets <102356010+Benti261@users.noreply.github.com>
2024-12-26 07:54:42 +0300

    Add files via upload

---
+++ tests/test_circle.py
@@ -0,0 +1,16 @@
+import pytest
+from square import area, perimeter
+
+def test_area_positive():
+    assert area(4) == 16
+
+def test_area_negative():
+    with pytest.raises(ValueError):
+        area(-4)
+
+def test_perimeter_positive():
+    assert perimeter(4) == 16
+
+def test_perimeter_negative():
+    with pytest.raises(ValueError):
+        perimeter(-4)
