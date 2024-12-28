3ee0768c3fe7c33bf791af1f604615aa96da0807
Vlad Krivulets <102356010+Benti261@users.noreply.github.com>
2024-12-28 14:57:14 +0300

    Update test_square.py

--- tests/test_square.py
+++ tests/test_square.py
@@ -1,10 +1,10 @@
-
 from ..square import area, perimeter
 
 def test_area():
     assert area(1) == 1
     assert area(3) == 9
     assert area(5) == 25
+
 
 def test_perimeter():
     assert perimeter(1) == 4
